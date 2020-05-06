2290861974f9110f9038bc5cdc6b48d7d1b72b5e
jubaan <julio.ab@outlook.com>
2020-05-06 17:58:05 -0500

    Update author model, change validation length name and add has_many to comments relationship

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,8 +2,9 @@
 
 class Author < ApplicationRecord
   has_many :posts
+  has_many :comments
 
-  validates_length_of :name, within: 6..20, too_long: ': pick a shorter name', too_short: ': pick a longer name'
+  validates_length_of :name, within: 3..20, too_long: ': pick a shorter name', too_short: ': pick a longer name'
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true
 end
