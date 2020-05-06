5f1b3bef44c45d21fea3c9db8a06b73d91a32293
jubaan <julio.ab@outlook.com>
2020-05-06 15:25:50 -0500

    Create validations for user

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,7 @@
 class Author < ApplicationRecord
   has_many :posts
+
+  validates_length_of :name, :within => 6..20, :too_long => "pick a shorter name", :too_short => "pick a longer name"
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true
 end
